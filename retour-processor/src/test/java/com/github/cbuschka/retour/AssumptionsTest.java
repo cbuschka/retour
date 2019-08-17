3b499277ce0869520f76db6b91732426df024647
conni <cbuschka@gmail.com>
2019-08-17 19:56:40 +0200

    Added test if empty map serializes to json.

--- retour-processor/src/test/java/com/github/cbuschka/retour/AssumptionsTest.java
+++ retour-processor/src/test/java/com/github/cbuschka/retour/AssumptionsTest.java
@@ -1,7 +1,12 @@
 package com.github.cbuschka.retour;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 
@@ -11,5 +16,12 @@ public class AssumptionsTest {
 		String javaVersion = System.getProperty("java.version");
 
 		assertThat(javaVersion, startsWith("1.8."));
+	}
+
+	@Test
+	public void emptyMapSerializes() throws JsonProcessingException {
+		String json = new ObjectMapper().writeValueAsString(Collections.emptyMap());
+
+		assertThat(json, is("{}"));
 	}
 }
