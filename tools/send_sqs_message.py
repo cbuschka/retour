6ae3ac02ff5e626210db629b7eb9dac419245cff
conni <cbuschka@gmail.com>
2019-08-19 20:54:17 +0200

    Added suffix support to send_sqs_message.py.

--- tools/send_sqs_message.py
+++ tools/send_sqs_message.py
@@ -1,13 +1,19 @@
 import boto3
 from time import gmtime, strftime
 import json
+import sys
 
 sqs = boto3.resource('sqs')
 
 queue = sqs.get_queue_by_name(QueueName='retour')
 
-retourNo = "retour"+strftime("%Y%m%d%H%M%S", gmtime())
-retourJson = json.dumps({"retourNo":retourNo,"orderNo":"O1234"})
+if len(sys.argv) == 2:
+  suffix = sys.argv[1]
+else:
+  suffix = strftime("%Y%m%d%H%M%S", gmtime())
+retourNo = "R"+suffix
+orderNo = "O"+suffix
+retourJson = json.dumps({"retourNo":retourNo,"orderNo":orderNo})
 response = queue.send_message(MessageBody=retourJson)
 
 print("Sent {} with messageId={}.".format(retourJson, response.get('MessageId')))
